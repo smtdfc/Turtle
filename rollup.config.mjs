a991535a4ea2ddbcc197da823a309e23042a5e27
smtdfc.master@gmail.com <smtdfc.master@gmail.com>
2023-09-03 16:06:32 +0700

    Remove StorageModule

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,14 +4,17 @@ export default  {
   input:"./src/turtle.js" ,
   output: [
     {
-      file: './dist/turtle.cjs.js',
+      file: './dist/turtle.min.cjs',
       format: 'cjs'
     },
     {
       file: './dist/turtle.min.js',
       format: 'iife'
-
-    }
+    },
+        {
+          file: './dist/turtle.min.mjs',
+          format: 'esm'
+        },
   ],
   plugins:[terser()]
 };
