f9105ac1ac7f8f7d146523308a1adabe6214d844
smtdfc <smtdfc.master@gmail.com>
2024-02-11 09:31:15 +0700

    update turtle.js

--- src/turtle.js
+++ src/turtle.js
@@ -1,5 +1,22 @@
 import {} from './components/base.js';
 export * from "./components/component.js"
-export {render} from "./components/render.js"
+export { render } from "./components/render.js"
 export * from "./modules/router/index.js"
 export * from "./app.js"
+
+export function importScript(d, src, asyncLoad=false,script) {
+  return new Promise((resolve, reject) => {
+    script = d.createElement('script');
+    script.type = 'text/javascript';
+    script.async = asyncLoad;
+    script.onload = function() {
+      resolve()
+    };
+    script.onerr = function() {
+      reject()
+    }
+    script.src = src;
+    d.getElementsByTagName('head')[0].appendChild(script)
+  })
+
+}
