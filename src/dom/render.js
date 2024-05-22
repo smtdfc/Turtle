51ea27e8f33aa8c86e5c68c695ac74d5b9d877b9
smtdfc <smtdfc.master@gmail.com>
2024-05-23 01:44:06 +0700

    update render.js

--- src/dom/render.js
+++ src/dom/render.js
@@ -1,29 +1,30 @@
 import { TurtleComponent } from '../component/component.js';
 import { parseHTML } from './parser.js';
-import {process} from './processor.js';
+import { process } from './processor.js';
 
-export function render(raw, values,ctx) {
+export function render(raw, values, ctx) {
   let data = {
     components: {}
   }
-  
+
 
   for (let i = 0; i < values.length; i++) {
-    
+
     let key = `turtle-component-${(Math.floor(Math.random() * 999999) * Date.now()).toString(16)}`
-    
-    if (values[i] instanceof TurtleComponent) {
-      data.components[key] = values[i]
-      values[i] = key
-    } else if (values[i].ins === TurtleComponent) {
-      data.components[key] = values[i]()
-      values[i] = key
+    if (values[i] !== undefined) {
+      if (values[i] instanceof TurtleComponent) {
+        data.components[key] = values[i]
+        values[i] = key
+      } else if (values[i].ins === TurtleComponent) {
+        data.components[key] = values[i]()
+        values[i] = key
+      }
     }
   }
-  
-  let content = String.raw(raw,...values)
+
+  let content = String.raw(raw, ...values)
   let doc = parseHTML(content)
   let root = document.createDocumentFragment()
-  process(root,doc,data,ctx)
+  process(root, doc, data, ctx)
   return root
 }
