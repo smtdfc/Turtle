85568d074b4610c2bb11d9b64e45536e79511348
smtdfc <smtdfc.master@gmail.com>
2024-10-19 08:21:00 +0700

    add event

--- src/context/index.js
+++ src/context/index.js
@@ -1,43 +1,56 @@
-export class TurtleContext{
-  constructor(){
+export class TurtleContext {
+  constructor() {
     this.values = {}
     this._bindings = {}
+    this._events = {}
   }
-  
-  initValue(value){
+
+  on(name, callback) {
+    if (!this._events[name]) this._events[name] = []
+    this._events.push(callback)
+  }
+
+  emit(name, data) {
+    if (!this._events[name]) this._events[name] = []
+    this._events.forEach(callback => {
+      callback(data)
+    })
+  }
+
+
+  initValue(value) {
     this.values = value
   }
-  
-  initState(name,value){
+
+  initState(name, value) {
     this.values[name] = value
   }
-  
-  get(name){
+
+  get(name) {
     return this.values[name]
   }
-  
-  set(name,value){
+
+  set(name, value) {
     this.values[name] = value
-    this._reactive(name,value)
+    this._reactive(name, value)
   }
-  
-  bind(key,state){
-    this._binding(key,state)
+
+  bind(key, state) {
+    this._binding(key, state)
   }
-  
-  _reactive(key,value){
-    
-    if(!this._bindings[key]) this._bindings[key]=[]
-    this._bindings[key].forEach(state=>{
+
+  _reactive(key, value) {
+
+    if (!this._bindings[key]) this._bindings[key] = []
+    this._bindings[key].forEach(state => {
       state.set(value)
     })
   }
-  
-  
-  _binding(key,state){
+
+  _binding(key, state) {
     let component = state._component
-    if(!this._bindings[key]) this._bindings[key]=[]
+    if (!this._bindings[key]) this._bindings[key] = []
     this._bindings[key].push(state)
   }
-  
+
 }
