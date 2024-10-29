530d4093102a0040e9c95c141b05457238ed9ccc
smtdfc <135759660+smtdfc@users.noreply.github.com>
2024-10-29 17:58:15 +0700

    Delete test 1 directory

--- test 1/src/components/AppContainer.js
+++
@@ -1,9 +0,0 @@
-class Component extends Turtle.TurtleComponent {
-  template() {
-    return this.html`
-      <div id="content"/>
-    `
-  }
-}
-
-export const AppContainer = Turtle.createComponent(Component)
