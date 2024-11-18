93464414c0195c4e5023e35de76e1be52fc98844
smtdfc <smtdfc.master@gmail.com>
2024-11-18 11:01:16 +0700

    update lazy.js

--- src/features/lazy.js
+++ src/features/lazy.js
@@ -1,28 +1,26 @@
 import { TurtleComponent, createComponent } from "../component/component.js";
 
-
-
 export class TurtleLazyLoadingComponent extends TurtleComponent {
-    onCreate() {
-        if (this.props[0]) {
-            this.lazy_fn = this.props[0]
-        }
+  onCreate() {
+    if (this.props[0]) {
+      this.lazy_fn = this.props[0]
     }
+  }
 
-    onRender() {
+  onRender() {
 
-        let context = this
-        this.lazy_fn()
-            .then((component) => {
-                context.element.appendChild(this.html`
+    let context = this
+    this.lazy_fn()
+      .then((component) => {
+        context.element.appendChild(this.html`
                  <${component}/>
                 `)
-            })
-    }
+      })
+  }
 
-    template() {
-        return this.html``
-    }
+  template() {
+    return this.html``
+  }
 
 }
 
