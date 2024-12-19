7f0240704d07d11f9f5f362b20a74fcf5b8193c7
smtdfc <smtdfc.master@gmail.com>
2024-12-19 17:14:29 +0700

    feat(turtle_features): add script load

---
+++ src/features/loader/index.js
@@ -0,0 +1,36 @@
+function isNamespaceAvailable(namespace) {
+  return namespace.split('.').reduce((acc, part) => {
+    if (acc && acc[part]) {
+      return acc[part];
+    }
+    return null;
+  }, window) !== null;
+}
+
+export function loadScript(path, options = {}) {
+  const { defer = false, async_ = false, module = false } = options;
+
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = path;
+    script.defer = defer;
+    script.async = async_;
+    if (module) script.type = "module";
+
+    script.onload = () => resolve();
+    script.onerror = (err) => reject(err);
+
+    document.body.appendChild(script);
+  });
+}
+
+
+
+export function loadScriptIfNeeded(path, namespace, options = {}) {
+  if (isNamespaceAvailable(namespace)) {
+    return Promise.resolve();
+  }
+
+  return loadScript(path, options);
+}
+
